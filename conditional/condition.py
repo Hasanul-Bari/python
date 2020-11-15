a9e2fd894f3c20f70b719522387af9a0257b2238
Hasanul Bari <hasanul.bari.hasan96@gmail.com>
2020-11-15 20:52:21 +0600

    Create condition.py

---
+++ conditional/condition.py
@@ -0,0 +1,19 @@
+x=42
+if x>1:
+    print('More than 1')
+    print('Still bigger')
+    if x<100:
+        print('Less than 100')
+else :
+    print('Less than 1')
+print('All done')
+
+
+#else if
+x=1
+if x>2:
+    print('More than 2')
+elif x<2:
+    print('Less than 2')
+else :
+    print('Equal to 2')
