850e58039f4b01dfc3b0c2d31976c88e9427fb80
Hasanul Bari <hasanul.bari.hasan96@gmail.com>
2020-11-16 21:22:33 +0600

    Create lists with string.py

---
+++ lists/lists with string.py
@@ -0,0 +1,18 @@
+#split string
+
+abc='With three words'
+stuff=abc.split()
+print(stuff)
+print(len(stuff))
+print(stuff[1])
+
+
+
+s = 'spam-spam-spam'
+list=s.split('-')
+print(list)
+
+
+t = ['pining', 'for', 'the', 'fjords']
+list='-'.join(t)
+print(list)
