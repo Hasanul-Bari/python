fbcaac186399c7ededad47412f3a4d8306171ee9
Hasanul Bari <hasanul.bari.hasan96@gmail.com>
2020-11-20 19:42:37 +0600

    Create json2.py

---
+++ parsing json/json2.py
@@ -0,0 +1,25 @@
+import json
+
+#[] -> list       {} -> dict
+data='''
+[
+    {
+        "id" : "001",
+        "x" : "2",
+        "name" : "Hasan"
+    },
+    {
+        "id" : "009",
+        "x" : "7",
+        "name" : "Hasanul"
+    }
+]'''
+        
+info=json.loads(data)
+
+print('User count:', len(info))
+
+for item in info:
+    print('Name:', item['name'])
+    print('Id:', item['id'])
+    print('Attribute:', item['x'])
