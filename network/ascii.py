67c26e339c7f7737cccccee7b1e5f2c88605389b
Hasanul Bari <hasanul.bari.hasan96@gmail.com>
2020-11-22 21:03:44 +0600

    Create ascii.py

---
+++ network/ascii.py
@@ -0,0 +1,16 @@
+print(ord('t'))
+print(ord('e'))
+print(ord('\n'))
+
+x=b'abc'               #byte string
+print(type(x))
+
+x='বাংলাদেশ'            #normal old regular atring
+print(type(x))
+
+x=u'বাংলাদেশ'           #unicode string
+print(type(x))
+
+
+
+#inside python 3 everything is unicode
